30fa6be32b7a3ae1fd810f2c77504d0b2e1dda8e
7jets <zhaoruoyan@gmail.com>
2018-10-31 21:07:24 +0800

    Update addMaxNumber.cpp

--- addMaxNumber.cpp
+++ addMaxNumber.cpp
@@ -26,17 +26,14 @@ int main(){
 	}
 	for(int i = 0;i<aSize;i++){
 		aa[i] = a[i] - '0';
-		cout<<aa[i]<<endl;
 	}
 	for(int i = 0;i<bSize;i++){
 		bb[i] = b[i] - '0';
-		cout<<bb[i]<<endl;
 	}
 	
 	for(int i = 0;i<aSize;i++){
 		for(int j = 0;j<bSize;j++){
 			result[cSize-i-j-2] = result[cSize-i-j-2] + aa[i]*bb[j];
-			cout<<cSize-i-j-2<<","<<result[cSize-i-j-2]<<endl;
 		}
 	}
 	
@@ -47,7 +44,13 @@ int main(){
 		result[i] = temp1;
 	}
 	
+	int first = 1;
 	for(int i = cSize-1;i>=0;i--){
+		if(result[i] == 0 && first){
+			first = 0;
+		}else{
+	
 		cout<<result[i];
+		}
 	}
 }
